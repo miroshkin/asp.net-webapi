f065eefcea6a06d857d9f96b41cf4dee18e4017d
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-03-12 10:19:17 +0300

    Added NuGet packages

---
+++ Treehouse.FitnessFrog.Spa/App_Start/WebApiConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace Treehouse.FitnessFrog.Spa
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+
+        }
+    }
+}
