339cf412fea86a059b1f68435b94a70094bbad3f
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-03-12 10:22:25 +0300

    Added default API route.

--- Treehouse.FitnessFrog.Spa/App_Start/WebApiConfig.cs
+++ Treehouse.FitnessFrog.Spa/App_Start/WebApiConfig.cs
@@ -6,7 +6,11 @@ namespace Treehouse.FitnessFrog.Spa
     {
         public static void Register(HttpConfiguration config)
         {
-
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults:new {id = RouteParameter.Optional}
+                );
         }
     }
 }
